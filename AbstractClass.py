ba9a0efa047562d25dd2760c9f0f8eae28010310
Bryan Ramses Garcia <44005842+bryangarciad@users.noreply.github.com>
2023-12-08 10:00:46 -0800

    Update AbstractClass.py

--- AbstractClass.py
+++ AbstractClass.py
@@ -1,33 +1,37 @@
-#In the core of the python language, abstract classes are not represented, 
-# the first ones were implemented in the standard abc module
+# Abstraction in object-oriented programming (OOP) is the concept of hiding the complex implementation details 
+# and showing only the necessary features of an object. It allows you to focus on what an object does rather than how it does it.
 
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
 
-class Movable(ABC):
+# Abstract class
+class Shape(ABC):
+    def __init__(self, name):
+        self.name = name
 
     @abstractmethod
-    def move(self):
-        return "Move"
-
-    @abstractproperty
-    def speed(self):
+    def area(self):
         pass
 
+# Concrete classes inheriting from Shape
+class Circle(Shape):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
 
-class Car(Movable):
-    speed = 200
-    def move(self):
-        return "Car move method"
+    def area(self):
+        return 3.14 * self.radius * self.radius
 
-car = Car()
-print(car.move())
+class Square(Shape):
+    def __init__(self, name, side):
+        super().__init__(name)
+        self.side = side
 
+    def area(self):
+        return self.side * self.side
 
+# Using the classes
+circle = Circle("Circle", 5)
+square = Square("Square", 4)
 
-
-
-
-
-
-
-
+print(circle.area())  # Output: 78.5
+print(square.area())  # Output: 16
