18cb12650b48112adb9f7647cfd6b61e87a410a8
Bryan Ramses Garcia <44005842+bryangarciad@users.noreply.github.com>
2023-12-08 10:03:07 -0800

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,26 +1,37 @@
+# Encapsulation in Object-Oriented Programming (OOP) is a fundamental principle that involves bundling 
+# the data (attributes) and methods (functions) that operate on the data within a single unit, known 
+# as a class. 
+# It helps in controlling the access to the inner workings of an object, 
+# providing a way to protect its integrity and prevent unauthorized access.
+
 class Car:
+    def __init__(self, make, model, year):
+        self._make = make  # Protected attribute
+        self._model = model  # Protected attribute
+        self.__year = year  # Private attribute
 
-    #Constructor
-    def __init__(self, model):
-        self.model = model
+    def display_details(self):
+        return f"Car: {self._make} {self._model} ({self.__year})"
 
-    #Model property
-    @property
-    def model(self):
-        return self.__model
+    def update_year(self, new_year):
+        self.__year = new_year  # Method to update private attribute
 
-    #Model setter for poperty (kinda data filter)
-    @model.setter
-    def model(self, model):
-        if model < 2000:
-            self.__model = 2000
-        elif model > 2018:
-            self.__model = 2018
-        else:
-            self.__model = model
 
-    def getCarModel(self):
-        return "Model release year" + str(self.model)
+# Creating a Car object
+my_car = Car("Toyota", "Corolla", 2020)
 
-carA = Car(2008)
-print(carA.getCarModel())
+# Accessing attributes (public and protected)
+print(my_car._make)  # Accessing protected attribute
+# Output: Toyota
+
+# Trying to access private attribute directly (won't work due to name mangling)
+# print(my_car.__year)  # This will cause an AttributeError
+
+# Accessing private attribute using name mangling (not recommended)
+print(my_car._Car__year)  # Name mangling to access private attribute
+# Output: 2020
+
+# Using methods to access and modify private attribute
+print(my_car.display_details())  # Output: Car: Toyota Corolla (2020)
+my_car.update_year(2022)
+print(my_car.display_details())  # Output: Car: Toyota Corolla (2022)
